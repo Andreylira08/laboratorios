3be4c1228f24cd19994f0c2fc5b377c64507597f
Andrey <enriqueandrey@hotmail.com>
2025-02-27 08:55:51 -0600

    Correciones

--- LaboratorioCool/app.js
+++ LaboratorioCool/app.js
@@ -1,56 +1,27 @@
-
-
 const express = require('express');
 const app = express();
 
-const bodyParser= require('body-parser');
+const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({extended: false})); 
-
-const plantas= [];
+app.use(bodyParser.urlencoded({extended: false}));
 
 //Middleware
-
 app.use((request, response, next) => {
     console.log('Middleware!');
 
-
-    next(); //Le permite a la petición avanzar hacia el siguiente middleware
-
+    //Le permite a la petición avanzar hacia el siguiente middleware
+    next(); 
 });
 
-//app.get para registrar un middleware para peticiones HTTP GET 
+const plantasRoutes = require('./routes/plantas.routes');
 
-const plantasRoutes = require('./routes/plantas.routes'); 
-
-app.use('/plantas',plantasRoutes)
-
-app.get('/plantas/agregar',(request,response,next) => {                  //Registrar un middleware 
-    
-    response.send(html_header + html_form + html_footer); 
-});
-
-//app.post
-
-app.post('/plantas/agregar',(request,response,next) => {
-    console.log(request.body);
-    plantas.push(request.body.nombre);
-    let html=html_header; 
-    for(let planta of plantas){
-   
-    html += html_footer
-    
-}
-    response.send(html_header + html_form + html_footer); 
-});
-
-
+app.use('/plantas', plantasRoutes);
 
 app.use((request, response, next) => {
     console.log('Otro middleware!');
-    response.send('¡Hola mundo!'); //Manda la respuesta
+    
+    //Manda la respuesta
+    response.send('¡Hola mundo!'); 
 });
 
-
 app.listen(3000);
-                      
--- LaboratorioCool/routes/plantas.routes.js
+++ LaboratorioCool/routes/plantas.routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const app = express();
+
+const router = express.Router();
 
 const html_header = `
 <!DOCTYPE html>
@@ -116,4 +117,33 @@ const html_footer = `</div>
 </html>
 `;
 
+const plantas = [];
 
+//router.get es para registrar un middleware para peticiones HTTP GET
+router.get('/agregar', (request, response, next) => {
+    response.send(html_header + html_form + html_footer);
+});
+
+//router.post es para registrar un middleware para peticiones HTTP POST
+router.post('/agregar', (request, response, next) => {
+    console.log(request.body);
+    plantas.push(request.body.nombre);
+    let html = html_header;
+    html += `<div class="columns">`;
+    for(let planta of plantas) {
+        html += `<div class="column">`;
+        html += `<div class="card">
+        <div class="card-content">
+          <div class="content">`;
+        html += planta;
+        html += `</div>
+                </div>
+              </div>
+            </div>`;
+    }
+    html += `</div>`;
+    html += html_footer;
+    response.send(html);
+});
+
+module.exports = router;
