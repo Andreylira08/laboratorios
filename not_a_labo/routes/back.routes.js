e8d791994dd74255d3273558a865e3b08d57c1f7
Andrey <enriqueandrey@hotmail.com>
2025-03-16 17:19:25 -0600

    Protegiendo nuestras rutas

--- not_a_labo/routes/back.routes.js
+++ not_a_labo/routes/back.routes.js
@@ -1,17 +1,21 @@
+const { response, request } = require('express');
 const express = require('express');
 const router = express.Router();
+
+const isAuth = require('../util/is-auth');
+
 
 const back_controller = require('../controllers/back.controller');
 
 //router.get es para registrar un middleware para peticiones HTTP GET, para que un middle ware exista necesita una funcion anónima
 
 
-router.get('/', back_controller.get_back); 
+router.get('/', isAuth,back_controller.get_back); 
 
-router.post('/agregar', back_controller.post_agregar); 
+router.post('/agregar',isAuth, back_controller.post_agregar); 
 
-router.get('/:id', back_controller.get_nombres);
+router.get('/:id',isAuth, back_controller.get_nombres);
 
-router.get('/nombres',back_controller.get_nombres)
+router.get('/nombres',isAuth,back_controller.get_nombres)
 
 module.exports = router;
--- not_a_labo/routes/menu.routes.js
+++ not_a_labo/routes/menu.routes.js
@@ -1,12 +1,16 @@
+const { response, request } = require('express');
 const express = require('express');
 const router = express.Router();
+
+const isAuth = require('../util/is-auth');
+
 
 const menu_controller = require('../controllers/menu.controller');
 
 //ROUTER.GET RECIBE MIDDLEWARE PETICIONES HTTP GET  
 
-router.get('/', menu_controller.get_menu); 
+router.get('/',isAuth, menu_controller.get_menu); 
 
-router.get("/perrito",menu_controller.get_perrito);
+router.get("/perrito",isAuth,menu_controller.get_perrito);
   
 module.exports = router;   
