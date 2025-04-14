3ecfdb94b0280162b46b3721f21e527a6f9b0d31
Andrey <enriqueandrey@hotmail.com>
2025-04-13 19:36:34 -0600

    Transacciones - triggers

--- Labo28/trigger28.sql
+++ Labo28/trigger28.sql
@@ -14,3 +14,53 @@ END IF;
 -- Opcional: asignar fecha actual a la entrega 
 
 SET NEW.fecha = NOW(); END;
+
+--TRIGER 2 
+
+DELIMITER //
+
+CREATE TRIGGER tr_valida_cantidad_positiva
+BEFORE INSERT ON Entregan
+FOR EACH ROW
+BEGIN
+    IF NEW.Cantidad <= 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Error: La cantidad debe ser mayor a cero.';
+    END IF;
+END //
+
+DELIMITER ;
+
+--PRUEBA
+
+-- Insertar entrega con cantidad negativa 
+INSERT INTO Entregan VALUES (9999, 'AAAA800101', 5000, '15/12/2023', -100);
+-- Mensaje: Error: La cantidad debe ser mayor a cero.
+
+
+--TRIGER 3
+
+DELIMITER //
+
+CREATE TRIGGER tr_protege_proyectos_con_entregas
+BEFORE DELETE ON Proyectos
+FOR EACH ROW
+BEGIN
+    DECLARE v_entregas INT;
+    
+    -- Verificar si el proyecto tiene entregas
+    SELECT COUNT(*) INTO v_entregas
+    FROM Entregan
+    WHERE numero = OLD.Numero;
+    
+    IF v_entregas > 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Error: No se puede eliminar un proyecto con entregas registradas.';
+    END IF;
+END //
+
+DELIMITER ;
+
+-- Intentar eliminar el proyecto "Vamos Mexico" 
+DELETE FROM Proyectos WHERE Numero = 5000;
+-- Mensaje: Error: No se puede eliminar un proyecto con entregas registradas.
